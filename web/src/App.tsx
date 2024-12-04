d852e5049d14f67b2b62060b4723d284465b6b22
Giovana Napoli <giovana.ap.napoli@outlook.com>
2024-12-04 18:29:47 -0300

    init projects

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-blue-700 font-bold">Hello word</h1>
+    </div>
+  );
+}
+
+export default App;
