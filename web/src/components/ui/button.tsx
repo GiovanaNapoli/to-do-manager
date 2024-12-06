9ea92ef76bc7a19556f9dad34bf83db051526a1d
Giovana Napoli <giovana.ap.napoli@outlook.com>
2024-12-05 22:36:41 -0300

    feat: change cursor when button is disabled

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -8,7 +8,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       <button
         ref={ref}
         {...props}
-        className="disabled:bg-purple-400 flex h-12 items-center justify-center gap-2 rounded-lg text-sm font-medium tracking-tight outline-none ring-offset-2 ring-offset-black focus-visible:ring-2 bg-violet-500 text-violet-50 hover:bg-violet-600 ring-violet-500"
+        className="disabled:bg-purple-400 disabled:cursor-not-allowed flex h-12 items-center justify-center gap-2 rounded-lg text-sm font-medium tracking-tight outline-none ring-offset-2 ring-offset-black focus-visible:ring-2 bg-violet-500 text-violet-50 hover:bg-violet-600 ring-violet-500"
       />
     );
   }
