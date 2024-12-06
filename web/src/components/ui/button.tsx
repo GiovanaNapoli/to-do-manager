a3b2d9299159ea229f22a48eee7961d3a79dc7d3
Giovana Napoli <giovana.ap.napoli@outlook.com>
2024-12-05 22:39:53 -0300

    feat: remove forwardRef

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -1,15 +1,12 @@
-import { forwardRef, type ComponentProps } from "react";
+import type { ComponentProps } from "react";
 
 type ButtonProps = ComponentProps<"button">;
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  (props, ref) => {
-    return (
-      <button
-        ref={ref}
-        {...props}
-        className="disabled:bg-purple-400 disabled:cursor-not-allowed flex h-12 items-center justify-center gap-2 rounded-lg text-sm font-medium tracking-tight outline-none ring-offset-2 ring-offset-black focus-visible:ring-2 bg-violet-500 text-violet-50 hover:bg-violet-600 ring-violet-500"
-      />
-    );
-  }
-);
+export const Button = (props: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className="disabled:bg-purple-400 disabled:cursor-not-allowed flex h-12 items-center justify-center gap-2 rounded-lg text-sm font-medium tracking-tight outline-none ring-offset-2 ring-offset-black focus-visible:ring-2 bg-violet-500 text-violet-50 hover:bg-violet-600 ring-violet-500"
+    />
+  );
+};
--- web/src/components/ui/input.tsx
+++ web/src/components/ui/input.tsx
@@ -1,13 +1,12 @@
-import { forwardRef, type ComponentProps } from "react";
+import type { ComponentProps } from "react";
 
 type InputProps = ComponentProps<"input">;
 
-export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+export const Input = (props: InputProps) => {
   return (
     <input
       {...props}
-      ref={ref}
       className="px-4 h-12 bg-neutral-950 border border-zinc-900 rounded-lg placeholder-zinc-400 outline-none text-sm hover:border-zinc-800 focus-visible:border-pink-500 focus-visible:ring-4 ring-pink-500/10"
     />
   );
-});
+};
